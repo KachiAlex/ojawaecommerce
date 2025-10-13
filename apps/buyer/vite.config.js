8d6a6bfc1efede5e60d6d9814569c71a54f00316
Kachi Alex <opd.livmind@gmail.com>
2025-10-13 06:04:17 +0100

    Add Google Maps API key to vite config

--- apps/buyer/vite.config.js
+++ apps/buyer/vite.config.js
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  define: {
+    'import.meta.env.VITE_GOOGLE_MAPS_API_KEY': JSON.stringify('AIzaSyCw_5hgEojEOW1hAIewyb4TkyHTN2od-Yk'),
+  },
   build: {
     rollupOptions: {
       output: {
